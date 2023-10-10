6ac6198f6f46ae16155bce47bf266cd59ae5adce
DavidSantos004 <ds0299646@gmail.com>
2023-10-10 07:47:01 -0500

    feat: :sparkles: Se agregaron nuevos inputs a el formulario y estilos
    Se añadieron el input para que el usuario suba una imagen y tambien otro input para que agrege su numero de documento y se cambiaron cosas irrelevantes

---
+++ js/main.js
@@ -0,0 +1,22 @@
+const myForm = document.querySelector(".container")
+const nombre = document.querySelector("#name_input")
+const email = document.querySelector("#email_input")
+const telefono = document.querySelector("#telephone_input")
+const genero = document.querySelector("#subject_input")
+const nDocumento = document.querySelector("#document_input")
+const img = document.querySelector("#image-url-input")
+const linkedin = document.querySelector("#linkedin")
+const github = document.querySelector("#github")
+const hobbies = document.querySelector("#hoobies")
+const skills = document.querySelector("#Skills")
+const Info = document.querySelector("#message_input")
+const botonSubmit = document.querySelector("#form_button")
+
+
+api = "http://127.0.0.1:5010/perfiles"
+
+
+
+
+
+//
