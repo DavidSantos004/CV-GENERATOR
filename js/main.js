89c57da3e3938ed76cfa69cc062982121ebe97e1
DavidSantos004 <ds0299646@gmail.com>
2023-10-10 13:24:30 -0500

    feat: :beers: Cambios y implementaciones improvisadas
    intento de modularizacion

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+import {env} from "./config.js";
+
+
 const myForm = document.querySelector(".container")
 const nombre = document.querySelector("#name_input")
 const email = document.querySelector("#email_input")
@@ -11,12 +14,55 @@ const hobbies = document.querySelector("#hoobies")
 const skills = document.querySelector("#Skills")
 const Info = document.querySelector("#message_input")
 const botonSubmit = document.querySelector("#form_button")
-
-
-api = "http://127.0.0.1:5010/perfiles"
-
-
+const uri = `${env.ssl + env.hotsName}:${env.port}`;
+const config = {method: undefined, headers: {"Content-Type": "application/json"}};
 
 
 
-//
+export const getAll = async()=>{
+    config.method = "GET";
+    // config.body = "";
+    let res = await (await fetch(`${uri}/perfiles`, config)).json();
+    return res;
+}
+
+
+// botonSubmit.addEventListener("click", () => {
+//   const userName = nombre.textContent;
+//   const userEmail = email.textContent;
+//   const userTelefono = telefono.textContent;
+//   const userGenero = genero.textContent;
+//   const userNDocumento = nDocumento.textContent;
+//   const userImg = img.textContent;
+//   const userLinkedin = linkedin.textContent;
+//   const userGithub = github.textContent;
+//   const userSkills = skills.textContent;
+//   const userInfo = Info.textContent;
+// });
+
+// const userData = {
+//   nombre: userName,
+//   email: userEmail,
+//   telefono: userTelefono,
+//   documento: userNDocumento,
+//   img: userImg,
+//   linkedin: userLinkedin,
+//   github: userGithub,
+//   skills: userSkills,
+//   info: userInfo,
+// };
+
+// fetch(api, {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+//   body: JSON.stringify(userData),
+// })
+//   .then((response) => response.json())
+//   .then((data) => {
+//     console.log("Datos guardados exitosamente:", data);
+//   })
+//   .catch((error) => {
+//     console.error("Error al guardar datos:", error);
+//   });
