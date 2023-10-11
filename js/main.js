9a9917fc33b72a55fffe8c89af3d4350b69f5ad4
DavidSantos004 <ds0299646@gmail.com>
2023-10-11 00:05:04 -0500

    feat: :sparkles: Se agregaron metodos de peticion HTTP y inputs
    se realizo el metodo POST y el metodo GET y se agregaron los ultimos inputs que se le pediran al usuario para realizar su CV

--- js/main.js
+++ js/main.js
@@ -1,68 +1,62 @@
 import {env} from "./config.js";
 
 
-const myForm = document.querySelector(".container")
-const nombre = document.querySelector("#name_input")
-const email = document.querySelector("#email_input")
-const telefono = document.querySelector("#telephone_input")
-const genero = document.querySelector("#subject_input")
-const nDocumento = document.querySelector("#document_input")
-const img = document.querySelector("#image-url-input")
-const linkedin = document.querySelector("#linkedin")
-const github = document.querySelector("#github")
-const hobbies = document.querySelector("#hoobies")
-const skills = document.querySelector("#Skills")
-const Info = document.querySelector("#message_input")
-const botonSubmit = document.querySelector("#form_button")
-const uri = `${env.ssl + env.hotsName}:${env.port}`;
-const config = {method: undefined, headers: {"Content-Type": "application/json"}};
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.getElementById("contact_form");
 
+    form.addEventListener("submit", function (event) {
+      event.preventDefault(); 
 
+      
+      const name = document.getElementById("name_input").value;
+      const email = document.getElementById("email_input").value;
+      const telephone = document.getElementById("telephone_input").value;
+      const subject = document.getElementById("subject_input").value;
+      const documentNumber = document.getElementById("document_input").value;
+      const imageUrl = document.getElementById("image-url-input").value;
+      const profesion = document.getElementById("profesion").value
+      const yearsExperience = document.getElementById("yearsExperience").value
+      const birthday = document.getElementById("Birthday").value;
+      const estudios = document.getElementById("estudios").value;
+      const linkedin = document.getElementById("linkedin").value;
+      const github = document.getElementById("github").value;
+      const hobbies = document.getElementById("hoobies").value;
+      const skills = document.getElementById("Skills").value;
+      const message = document.getElementById("message_input").value;
 
-export const getAll = async()=>{
-    config.method = "GET";
-    // config.body = "";
-    let res = await (await fetch(`${uri}/perfiles`, config)).json();
-    return res;
-}
-
-
-// botonSubmit.addEventListener("click", () => {
-//   const userName = nombre.textContent;
-//   const userEmail = email.textContent;
-//   const userTelefono = telefono.textContent;
-//   const userGenero = genero.textContent;
-//   const userNDocumento = nDocumento.textContent;
-//   const userImg = img.textContent;
-//   const userLinkedin = linkedin.textContent;
-//   const userGithub = github.textContent;
-//   const userSkills = skills.textContent;
-//   const userInfo = Info.textContent;
-// });
-
-// const userData = {
-//   nombre: userName,
-//   email: userEmail,
-//   telefono: userTelefono,
-//   documento: userNDocumento,
-//   img: userImg,
-//   linkedin: userLinkedin,
-//   github: userGithub,
-//   skills: userSkills,
-//   info: userInfo,
-// };
-
-// fetch(api, {
-//   method: "POST",
-//   headers: {
-//     "Content-Type": "application/json",
-//   },
-//   body: JSON.stringify(userData),
-// })
-//   .then((response) => response.json())
-//   .then((data) => {
-//     console.log("Datos guardados exitosamente:", data);
-//   })
-//   .catch((error) => {
-//     console.error("Error al guardar datos:", error);
-//   });
+     
+      const apiUrl = `${env.ssl + env.hotsName}:${env.port}/perfiles`;
+                    
+      //metodo POST:
+      fetch(apiUrl, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          id: documentNumber,
+          name,
+          email,
+          telephone,
+          subject,
+          imageUrl,
+          profesion,
+          estudios,
+          yearsExperience,
+          birthday,
+          linkedin,
+          github,
+          hobbies,
+          skills,
+          message,
+        }),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          console.log("Respuesta del servidor:", data);
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+        });
+    });
+  });
---
+++ js/portafolio.js
@@ -0,0 +1,59 @@
+import {env} from "./config.js";     
+const apiUrl = `${env.ssl + env.hotsName}:${env.port}/perfiles`;
+const perfilesContainer = document.getElementById("section-formularios")
+
+
+//metodo GET
+fetch(apiUrl)
+    .then((response) => {
+        if (!response.ok){
+            throw new Error(`No se pudo obtener los datos. Codigo de estado: ${response.status}`)
+        }
+        return response.json();
+    })
+    .then((data)=>{
+
+        data.forEach((perfil)=>{
+            const perfilElement = document.createElement("div")
+            perfilElement.innerHTML=`
+        <div class="container-fluid py-5" id="about">
+            <div class="container">
+                <div class="position-relative d-flex align-items-center justify-content-center">
+                    <h1 class="display-1 text-uppercase text-white" style="-webkit-text-stroke: 1px #dee2e6;">About</h1>
+                    <h1 class="position-absolute text-uppercase text-primary">About Me</h1>
+                </div>
+                <div class="row align-items-center">
+                    <div class="col-lg-5 pb-4 pb-lg-0" style="
+                    display: flex;
+                    justify-content: center;
+                ">
+                        <img class="img-fluid rounded w-100" src="${perfil.imageUrl}" alt="">
+                    </div>
+                    <div class="col-lg-7">
+                        <h3 class="mb-4">${perfil.profesion}</h3>
+                        <p>${perfil.message}</p>
+                        <div class="row mb-3">
+                            <div class="col-sm-6 py-2"><h6>Name: <span class="text-secondary">${perfil.name}</span></h6></div>
+                            <div class="col-sm-6 py-2"><h6>Genero: <span class="text-secondary">${perfil.subject}</span></h6></div>
+                            <div class="col-sm-6 py-2"><h6>Birthday: <span class="text-secondary">${perfil.birthday} </span></h6></div>
+                            <div class="col-sm-6 py-2"><h6>Estudios: <span class="text-secondary">${perfil.estudios} </span></h6></div>
+                            <div class="col-sm-6 py-2"><h6>Skills: <span class="text-secondary">${perfil.skills} </span></h6></div>
+                            <div class="col-sm-6 py-2"><h6>Experience: <span class="text-secondary">${perfil.yearsExperience} Years</span></h6></div>
+                            <div class="col-sm-6 py-2"><h6>Phone: <span class="text-secondary">${perfil.telephone}</span></h6></div>
+                            <div class="col-sm-6 py-2"><h6>Email: <span class="text-secondary">${perfil.email}</span></h6></div>
+                            <div class="col-sm-6 py-2"><h6>Linkedin: <span class="text-secondary"><a href="${perfil.linkedin} ">Dale un vistazo :)</a></span></h6></div>
+                            <div class="col-sm-6 py-2"><h6>Github: <span class="text-secondary"><a href="${perfil.github} ">Dale un vistazo :)</a></span></h6></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <hr>
+        <hr>
+            `
+            perfilesContainer.appendChild(perfilElement)
+        })
+    })
+.catch((error)=>{
+    console.error("Error", error)
+})
